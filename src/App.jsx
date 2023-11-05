fdac647282643eec3192ff401ae62b44054be36b
Thedarkness-bd <toisiftasrik@gmail.com>
2023-11-06 01:36:18 +0600

    file added

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      advice: "click on generate to get a quote",
+    };
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((res) => {
+        const { advice } = res.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          margin: "0 auto",
+          position: "relative",
+          top: "350px",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Quote Generator
+        </h1>
+
+        <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <Box sx={{ my: 15, mx: 2 }}>
+            <Typography
+              color="text.secondary"
+              variant="body1"
+              sx={{
+                fontSize: "30px",
+                textAlign: "center",
+                color: "navy",
+              }}
+              style={{
+                fontFamily: "Jost",
+              }}
+            >
+              {this.state.advice}
+            </Typography>
+          </Box>
+
+          <Box sx={{ mt: 3, ml: 1, mb: 1, textAlign: "center" }}>
+            <Button
+              onClick={this.fetchAdvice}
+              style={{
+                fontSize: "20px",
+              }}
+            >
+              Generate
+            </Button>
+          </Box>
+        </Box>
+      </div>
+    );
+  }
+}
+
+export default App;
